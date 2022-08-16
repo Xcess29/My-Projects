f42b526221b4c22484c09a121980cc6d08e9994d
Xcess29 <holisshellon29@gmail.com>
2022-08-16 01:08:34 +0100

    first commit

---
+++ Instagram/assets/js/main.js
@@ -0,0 +1,9 @@
+
+    var browserWindow = $(window);
+
+    // :: 1.0 Preloader Active Code
+    browserWindow.on('load', function () {
+        $('.preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    });
